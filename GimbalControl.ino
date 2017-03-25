ed81f7f49086d148219b591f1eb5a3d80bb5452e
hasanyasar07 <hasanyasar07@hotmail.com>
2017-03-25 20:39:56 +0300

    Add files via upload

---
+++ GimbalControl.ino
@@ -0,0 +1,24 @@
+
+#include <Servo.h>  /* Servo kutuphanesi projeye dahil edildi */
+Servo servoNesnesi;  /* servo motor nesnesi yaratildi */
+
+void setup()
+{
+  servoNesnesi.attach(3);  /* Servo motor 9 numarali pine baglandi */
+  Serial.begin(9600);
+}
+ 
+void loop()
+{
+
+ int sensorValue = analogRead(A0);
+ 
+  sensorValue=sensorValue/10;
+  sensorValue=map(sensorValue,30,90,60,115);
+  servoNesnesi.write(sensorValue);  /* Motorun mili 100. dereceye donuyor */
+  Serial.print("Ptn:");Serial.println(sensorValue);
+  
+  //delay(100);
+  
+}
+
